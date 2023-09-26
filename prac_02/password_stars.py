5deeb0bed32ff2d3ce5e9dcaa369f4b3242f6283
patrickkibii <patrickkiprotich.kibii@my.jcu.edu.au>
2023-09-26 16:19:52 +1000

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,21 @@
+MINIMUM_LENGTH = 6
+
+
+def main():
+    password = input("Password: ")
+    determine_valid_password(password, MINIMUM_LENGTH)
+    print_asterisks(password)
+
+
+def determine_valid_password(password, MINIMUM_LENGTH):
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid Password")
+        password = input("Password: ")
+    return password
+
+
+def print_asterisks(password):
+    print("*" * (len(password)))
+
+
+main()
