b639458e899a5b78e49aac6f2e1799b2662a2b2d
patrickkibii <patrickkiprotich.kibii@my.jcu.edu.au>
2023-09-26 16:27:13 +1000

    Add temperature convert program

---
+++ prac_02/temperatures.py
@@ -0,0 +1,8 @@
+def main():
+    celsius = int(input("celsius: "))
+    fahrenheit = int(input("fahrenheit: "))
+
+
+def convert_celsius_to_fahrenheit(celsius):
+
+def convert_fahrenheit_to_celsius(fahrenheit):
