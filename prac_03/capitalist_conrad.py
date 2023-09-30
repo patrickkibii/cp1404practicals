58e2d9171e7fa6bf0c2a73747ab3768910025195
Patrick <patrickkiprotich.kibii@my.jcu.edu.au>
2023-09-30 12:31:13 +1000

    Add prac 3 files

---
+++ prac_03/capitalist_conrad.py
@@ -0,0 +1,41 @@
+"""
+CP1404/CP5632 - Practical
+Capitalist Conrad wants a stock price simulator for a volatile stock.
+The price starts off at $10.00, and, at the end of every day there is
+a 50% chance it increases by 0 to 17.5%, and
+a 50% chance that it decreases by 0 to 5%.
+If the price rises above $100, or falls below $1.0, the program should end.
+The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
+"""
+import random
+
+MAX_INCREASE = 1.75  # 17.5%
+MAX_DECREASE = 0.05  # 5%
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
+INITIAL_PRICE = 10.0
+FILENAME = "stock.txt"
+
+
+out_file = open(FILENAME, 'w')
+
+price = INITIAL_PRICE
+print(f"Starting price: ${price:,.2f}")
+number_of_days = 0
+while MIN_PRICE <= price <= MAX_PRICE:
+    number_of_days += 1
+    price_change = 0
+    # generate a random integer of 1 or 2
+    # if it's 1, the price increases, otherwise it decreases
+    if random.randint(1, 2) == 1:
+        # generate a random floating-point number
+        # between 0 and MAX_INCREASE
+        price_change = random.uniform(0, MAX_INCREASE)
+    else:
+        # generate a random floating-point number
+        # between negative MAX_DECREASE and 0
+        price_change = random.uniform(-MAX_DECREASE, 0)
+
+    price *= (1 + price_change)
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+out_file.close()
---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+# print(random.randint(5,30)): 13, 24, 7, 16
+# print(random.randrange(3, 10, 2)): 5, 7, 7, 5, 3
+# print(random.uniform(2.5, 5.5)): 3.6191264237699112, 2.827928454558202, 5.245361061188906
+import random
+
+print(random.randint(0, 100))
