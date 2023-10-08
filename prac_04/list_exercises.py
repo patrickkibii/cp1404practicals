e4b6859274195727dc552320f404d7561ebf4667
Patrick <patrickkiprotich.kibii@my.jcu.edu.au>
2023-10-09 06:14:09 +1000

    Add Prac Files

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,18 @@
+numbers = []
+for i in range(5):
+    number = int(input("number: "))
+    numbers.append(number)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access Granted!")
+else:
+    print("Access Denied!")
--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,6 +9,8 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    subjects = get_subjects()
+    display_subject_details(subjects)
 
 
 def get_data():
@@ -24,6 +26,29 @@ def get_data():
         print(parts)  # See if that worked
         print("----------")
     input_file.close()
+
+
+def get_subjects():
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    subjects = []
+    input_file = open(FILENAME)
+    for line in input_file:
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(parts)  # See if that worked
+        print("----------")
+        subjects.append(parts)
+        return subjects
+    input_file.close()
+
+
+def display_subject_details(subjects):
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
 
 
 main()
